3409608ebd02ce9e99c052c805b1204a50ce1868
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-07 13:44:25 +0000

    init

---
+++ assets/script/script.js
@@ -0,0 +1,53 @@
+// SNACK 1
+const zucchine = [
+  {
+    varieta: 'africana',
+    peso: 400,
+    lunghezza: 30
+  },
+  {
+    varieta: 'spagnola',
+    peso: 300,
+    lunghezza: 20
+  },
+  {
+    varieta: 'tedesca',
+    peso: 200,
+    lunghezza: 10
+  },
+  {
+    varieta: 'belga',
+    peso: 100,
+    lunghezza: 15
+  },
+  {
+    varieta: 'rumena',
+    peso: 450,
+    lunghezza: 25
+  },
+  {
+    varieta: 'americana',
+    peso: 800,
+    lunghezza: 45
+  },
+  {
+    varieta: 'canadese',
+    peso: 345,
+    lunghezza: 50
+  },
+  {
+    varieta: 'brasiliana',
+    peso: 610,
+    lunghezza: 15
+  },
+  {
+    varieta: 'cinese',
+    peso: 120,
+    lunghezza: 5
+  },
+  {
+    varieta: 'russa',
+    peso: 750,
+    lunghezza: 38
+  }
+]
